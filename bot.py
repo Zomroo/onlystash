1d4ce55b1af4daa1b1447a9bdf41a5c2f42889c5
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-02-27 20:06:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -76,7 +76,7 @@ class Bot(Client):
                                           """)
         self.username = usr_bot_me.username
         #web-response
-        app = await web.Server(await web_server())
+        app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
         await web.TCPSite(app, bind_address, PORT).start()
