fe62a43bcc8fd00b57dad203de54525e36345d31
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-14 18:33:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ PORT = os.environ.get("PORT", "8080")
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "5148561602").split()):
+    for x in (os.environ.get("ADMINS", "5148561602 5413882446").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
